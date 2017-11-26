fbef50eb194983fa019f6eb7213eacc6367b8fb2
Vishal Jaggi <vishaljaggi@Vishals-MacBook-Pro.local>
2017-11-25 20:39:50 -0500

    indentation update for readability

--- js/script.js
+++ js/script.js
@@ -1,57 +1,57 @@
 
 $(document).on('pagecreate',function(){
-		const API_KEY = '8f6120ab5f4b328efa714467eb461276';
+	const API_KEY = '8f6120ab5f4b328efa714467eb461276';
 
-		init();
+	init();
 
-		function init(){
-			$('.ui-content').hide();
-			getPreferredCities();
-			loadHistory();
-			if(navigator.geolocation)
-			navigator.geolocation.getCurrentPosition(showPosition, showErr);
-			else
-				console.log("Geolocation is not supported");
-		}
+	function init(){
+		$('.ui-content').hide();
+		getPreferredCities();
+		loadHistory();
+		if(navigator.geolocation)
+		navigator.geolocation.getCurrentPosition(showPosition, showErr);
+		else
+		console.log("Geolocation is not supported");
+	}
 
-		//function to pass in city by name
-		function getWeatherDataForMyCity(city){
-			getWealtherInfo({cityName: city});
-			getForecastInfo({cityName: city});
-			$('.ui-content').show();
-		}
+	//function to pass in city by name
+	function getWeatherDataForMyCity(city){
+		getWealtherInfo({cityName: city});
+		getForecastInfo({cityName: city});
+		$('.ui-content').show();
+	}
 
-		//Functions for getting geo locations
-		function showPosition(position){
-			let myLocation= {};
-			myLocation.lat = position.coords.latitude;
-			myLocation.lng = position.coords.longitude;
-			console.log(position.coords.latitude + "," + position.coords.longitude);
-			getWealtherInfo({cords: myLocation});
-			getForecastInfo({cords: myLocation});
+	//Functions for getting geo locations
+	function showPosition(position){
+		let myLocation= {};
+		myLocation.lat = position.coords.latitude;
+		myLocation.lng = position.coords.longitude;
+		console.log(position.coords.latitude + "," + position.coords.longitude);
+		getWealtherInfo({cords: myLocation});
+		getForecastInfo({cords: myLocation});
 
-			$('.ui-content').show();
-		}
+		$('.ui-content').show();
+	}
 
-		function showErr(){
-			alert("Location should be turned on for accurate results")
-		}
+	function showErr(){
+		alert("Location should be turned on for accurate results")
+	}
 
-		function resetTable() {
-			$("#heading").text('');
-			$("#row").text('');
-			$("#descrip").text('');
-			$("#img").text('');
-		}
+	function resetTable() {
+		$("#heading").text('');
+		$("#row").text('');
+		$("#descrip").text('');
+		$("#img").text('');
+	}
 
-		/**
-		* Weather and Forecast
-		* APIs
-		* UI updates
-		**/
-		//Data parsing using ES6
-		function parseWeather(weatherInfo){
-	 	console.log(weatherInfo);
+	/**
+	* Weather and Forecast
+	* APIs
+	* UI updates
+	**/
+	//Data parsing using ES6
+	function parseWeather(weatherInfo){
+		console.log(weatherInfo);
 
 		let {name, main, weather} = weatherInfo;
 		let icon = `http://openweathermap.org/img/w/${weather[0].icon}.png`;
@@ -64,14 +64,14 @@ $(document).on('pagecreate',function(){
 
 	}
 
-		function getWealtherInfo({cords, cityName}){
+	function getWealtherInfo({cords, cityName}){
 		let apiUrl = cityName ?
-					`http://api.openweathermap.org/data/2.5/weather?
-					q=${cityName}
-					&appid=${API_KEY}&&units=metric`:
-					`http://api.openweathermap.org/data/2.5/weather?
-					lat=${cords.lat}&lon=${cords.lng}
-					&appid=${API_KEY}&&units=metric`;
+		`http://api.openweathermap.org/data/2.5/weather?
+		q=${cityName}
+		&appid=${API_KEY}&&units=metric`:
+		`http://api.openweathermap.org/data/2.5/weather?
+		lat=${cords.lat}&lon=${cords.lng}
+		&appid=${API_KEY}&&units=metric`;
 		$.ajax({
 			url:apiUrl,
 			type:'GET',
@@ -80,16 +80,16 @@ $(document).on('pagecreate',function(){
 		});
 	}
 
-		function parseForecast(data){
+	function parseForecast(data){
 
 		let [...forecastList] = data.list;
 
-        for(forecast of forecastList){
-        	let[date, time] = forecast.dt_txt.split(" ");
-        	forecast.date = date;
-    			forecast.time = time;
-        }
-        // For Next 5 days
+		for(forecast of forecastList){
+			let[date, time] = forecast.dt_txt.split(" ");
+			forecast.date = date;
+			forecast.time = time;
+		}
+		// For Next 5 days
 
 		resetTable();
 
@@ -107,163 +107,163 @@ $(document).on('pagecreate',function(){
 			}
 
 			i = i+7;
-    }
-        //Chart for next 7 hour intervals
-        let ctx = document.getElementById("forecastChart").getContext('2d');
-        let myChart = new Chart(ctx, {
-              type: 'line',
-	              data: {
-	                  labels: [forecastList[0].time,forecastList[1].time,forecastList[2].time,
-	                  forecastList[3].time,forecastList[4].time,forecastList[5].time,forecastList[6].time],
-		                datasets: [{
-		                  label: 'Temperature in Celsius',
-		                  data: [forecastList[0].main.temp.toFixed(1), forecastList[1].main.temp.toFixed(1),
-		                  forecastList[2].main.temp.toFixed(1), forecastList[3].main.temp.toFixed(1), forecastList[4].main.temp.toFixed(1), forecastList[5].main.temp.toFixed(1),
-		                   forecastList[6].main.temp.toFixed(1), forecastList[7].main.temp.toFixed(1)],
-		                  borderWidth: 1,
-		                  fill: false
-		            	}]
-	     		 },
-			      options: {
-			          scales: {
-			            yAxes: [{
-			              ticks: {
-			                beginAtZero:true
-			              }
-         			    }]
-        			  }
-    			  }
-        });
+		}
+		//Chart for next 7 hour intervals
+		let ctx = document.getElementById("forecastChart").getContext('2d');
+		let myChart = new Chart(ctx, {
+			type: 'line',
+			data: {
+				labels: [forecastList[0].time,forecastList[1].time,forecastList[2].time,
+				forecastList[3].time,forecastList[4].time,forecastList[5].time,forecastList[6].time],
+				datasets: [{
+					label: 'Temperature in Celsius',
+					data: [forecastList[0].main.temp.toFixed(1), forecastList[1].main.temp.toFixed(1),
+					forecastList[2].main.temp.toFixed(1), forecastList[3].main.temp.toFixed(1), forecastList[4].main.temp.toFixed(1), forecastList[5].main.temp.toFixed(1),
+					forecastList[6].main.temp.toFixed(1), forecastList[7].main.temp.toFixed(1)],
+					borderWidth: 1,
+					fill: false
+				}]
+			},
+			options: {
+				scales: {
+					yAxes: [{
+						ticks: {
+							beginAtZero:true
+						}
+					}]
+				}
+			}
+		});
 
 	}
 
-		function getForecastInfo({cords, cityName}){
-			let apiUrl = cityName ? `http://api.openweathermap.org/data/2.5/forecast?
-						q=${cityName}
-						&appid=${API_KEY}&&units=metric`
-						: `http://api.openweathermap.org/data/2.5/forecast?
-						lat=${cords.lat}&lon=${cords.lng}
-						&appid=${API_KEY}&&units=metric`;
-			$.ajax({
-				url:apiUrl,
-				type:'GET',
-				datatype: 'json',
-				success: parseForecast
-			});
-		}
-
-		/**
-		*Preffered City List
-		* on click event handler
-		**/
-		function parseCities(cityData){
-
-			//array destructuring ES6
-			let [...preferred] = cityData.preferred;
-
-			for (prefCity of preferred){
-				$('#cities').append(`<li><a href="#" data-transition="slide">${prefCity.city}, ${prefCity.country}</a></li>`);
-			}
-
-		}
-
-		function getPreferredCities(){
-			$.ajax({
-				url:'data/preferredCities.json',
-				type:'GET',
-				datatype: 'json',
-				success: parseCities,
-				complete: function() {
-							$('#cities').listview('refresh');
-					}
-			});
-		}
-
-		$('#cities a').on('click', function(){
-			console.log($(this).text());
-			getWeatherDataForMyCity($(element.target).text());
+	function getForecastInfo({cords, cityName}){
+		let apiUrl = cityName ? `http://api.openweathermap.org/data/2.5/forecast?
+		q=${cityName}
+		&appid=${API_KEY}&&units=metric`
+		: `http://api.openweathermap.org/data/2.5/forecast?
+		lat=${cords.lat}&lon=${cords.lng}
+		&appid=${API_KEY}&&units=metric`;
+		$.ajax({
+			url:apiUrl,
+			type:'GET',
+			datatype: 'json',
+			success: parseForecast
 		});
+	}
 
-		/**
-		* Auto Complete feature for city lookup
-		* on click event handler
-		**/
+	/**
+	*Preffered City List
+	* on click event handler
+	**/
+	function parseCities(cityData){
 
-		$( "#autocomplete" ).on( "filterablebeforefilter", function ( e, data ) {
-        var $ul = $( this ),
-            $input = $( data.input ),
-            value = $input.val(),
-            html = "";
-        $ul.html( "" );
-        if ( value && value.length > 2 ) {
-            $ul.html( "<li><div class='ui-loader'><span class='ui-icon ui-icon-loading'></span></div></li>" );
-            $ul.listview( "refresh" );
-            $.ajax({
-                url: "http://gd.geobytes.com/AutoCompleteCity",
-                dataType: "jsonp",
-                crossDomain: true,
-                data: {
-                    q: $input.val()
-                }
-            })
-            .then( function ( response ) {
-                $.each( response, function ( i, val ) {
-                    html += '<li><a href="#" data-transition="slide">' + val + '</a></li>';
-                });
-                $ul.html( html );
-                $ul.listview( "refresh" );
-                $ul.trigger( "updatelayout");
-            });
-        }
-    });
+		//array destructuring ES6
+		let [...preferred] = cityData.preferred;
 
-		$(document).on("click", "#autocomplete a", function () {
-					let searchResult = $(this).text();
-					getWeatherDataForMyCity(searchResult);
-					$(this).closest("ul").prev("form").find("input").val('');
-					historySearch(searchResult);
-		});
-
-		/**
-		* Local storage get/set and on click
-		**/
-
-		function historySearch(searchResult){
-			if(localStorage.getItem("history") === null){
-				localStorage.setItem("history", JSON.stringify([]));
-			}
-
-			let [cityName, ...country] = searchResult.split(',');
-
-			let city = {
-				 "city": cityName,
-				 "country": country[country.length - 1]
-			};
-			let searchHistory = JSON.parse(localStorage.getItem("history"));
-			searchHistory.push(city);
-			// store the data
-			localStorage.setItem("history", JSON.stringify(searchHistory));
-
-			//reload list
-			loadHistory();
+		for (prefCity of preferred){
+			$('#cities').append(`<li><a href="#" data-transition="slide">${prefCity.city}, ${prefCity.country}</a></li>`);
 		}
 
-		function loadHistory(){
-			$('#history').text('');
+	}
 
-			$('#history').append(`<li> Recent Searches </li>`);
-
-			let preferred = JSON.parse(localStorage.getItem("history"));
-
-			for (prefCity of preferred){
-				$('#history').append(`<li><a href="#" data-transition="slide">${prefCity.city}, ${prefCity.country}</a></li>`);
+	function getPreferredCities(){
+		$.ajax({
+			url:'data/preferredCities.json',
+			type:'GET',
+			datatype: 'json',
+			success: parseCities,
+			complete: function() {
+				$('#cities').listview('refresh');
 			}
-
-			$('#history').listview('refresh');
-		}
-
-		$(document).on("click", "#history a", function () {
-					getWeatherDataForMyCity($(this).text());
 		});
+	}
 
+	$('#cities a').on('click', function(){
+		console.log($(this).text());
+		getWeatherDataForMyCity($(element.target).text());
 	});
+
+	/**
+	* Auto Complete feature for city lookup
+	* on click event handler
+	**/
+
+	$( "#autocomplete" ).on( "filterablebeforefilter", function ( e, data ) {
+		var $ul = $( this ),
+		$input = $( data.input ),
+		value = $input.val(),
+		html = "";
+		$ul.html( "" );
+		if ( value && value.length > 2 ) {
+			$ul.html( "<li><div class='ui-loader'><span class='ui-icon ui-icon-loading'></span></div></li>" );
+			$ul.listview( "refresh" );
+			$.ajax({
+				url: "http://gd.geobytes.com/AutoCompleteCity",
+				dataType: "jsonp",
+				crossDomain: true,
+				data: {
+					q: $input.val()
+				}
+			})
+			.then( function ( response ) {
+				$.each( response, function ( i, val ) {
+					html += '<li><a href="#" data-transition="slide">' + val + '</a></li>';
+				});
+				$ul.html( html );
+				$ul.listview( "refresh" );
+				$ul.trigger( "updatelayout");
+			});
+		}
+	});
+
+	$(document).on("click", "#autocomplete a", function () {
+		let searchResult = $(this).text();
+		getWeatherDataForMyCity(searchResult);
+		$(this).closest("ul").prev("form").find("input").val('');
+		historySearch(searchResult);
+	});
+
+	/**
+	* Local storage get/set and on click
+	**/
+
+	function historySearch(searchResult){
+		if(localStorage.getItem("history") === null){
+			localStorage.setItem("history", JSON.stringify([]));
+		}
+
+		let [cityName, ...country] = searchResult.split(',');
+
+		let city = {
+			"city": cityName,
+			"country": country[country.length - 1]
+		};
+		let searchHistory = JSON.parse(localStorage.getItem("history"));
+		searchHistory.push(city);
+		// store the data
+		localStorage.setItem("history", JSON.stringify(searchHistory));
+
+		//reload list
+		loadHistory();
+	}
+
+	function loadHistory(){
+		$('#history').text('');
+		
+		$('#history').append(`<li> Recent Searches </li>`);
+
+		let preferred = JSON.parse(localStorage.getItem("history"));
+
+		for (prefCity of preferred){
+			$('#history').append(`<li><a href="#" data-transition="slide">${prefCity.city}, ${prefCity.country}</a></li>`);
+		}
+
+		$('#history').listview('refresh');
+	}
+
+	$(document).on("click", "#history a", function () {
+		getWeatherDataForMyCity($(this).text());
+	});
+
+});
